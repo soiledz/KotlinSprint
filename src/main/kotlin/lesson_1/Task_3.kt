1ce1c82c34dc40175c72d398414605b47c1718a7
varik <soiledz@gmail.com>
2024-09-16 22:48:54 +0300

    KS-1-task3 variables

---
+++ src/main/kotlin/lesson_1/Task_3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main(){
+    val yearFly = 1961
+    var hour = 9
+    var minute = 7
+
+    println(yearFly)
+    println("0$hour")
+    println("0$minute")
+
+    hour = 10
+    minute = 55
+
+    print("$hour:$minute")
+}
