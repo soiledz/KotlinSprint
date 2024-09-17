e3fb136e2ff0edd7998c8cfd57f84c393944d6df
varik <soiledz@gmail.com>
2024-09-18 00:15:12 +0300

    KS-2-4

---
+++ src/main/kotlin/lesson_2/KS-2-4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main() {
+    val baf = 20
+    val kristal = 7
+    val iron = 11
+
+    val kristalBaf = (kristal * baf) / 100
+    val ironBaf = (iron * baf) / 100
+
+    println(kristalBaf)
+    println(ironBaf)
+}
