438fd0ae22d4dc2320cf97df117d2d83b8aecbbc
varik <soiledz@gmail.com>
2024-09-18 23:56:10 +0300

    KS-1-5 create const

--- src/main/kotlin/lesson_1/Task_5.kt
+++ src/main/kotlin/lesson_1/Task_5.kt
@@ -1,10 +1,16 @@
 package org.example.lesson_1
 
 fun main() {
+
+    val SECONDS_IN_HOUR = 3600
+    val SECONDS_IN_MINUTE = 60
+
     val secondsInSpace: Short = 6365
-    val hour = secondsInSpace / 3600
-    val minute = (secondsInSpace - (hour * 3600)) / 60
-    val second = secondsInSpace % 60
+
+    val hour = secondsInSpace / SECONDS_IN_HOUR
+    val minute = (secondsInSpace - (hour * SECONDS_IN_HOUR)) / SECONDS_IN_MINUTE
+    val second = secondsInSpace % SECONDS_IN_MINUTE
+
     val timeInSpace = String.format("%02d:%02d:%02d", hour, minute, second)
 
     println(timeInSpace)
