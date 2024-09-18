c132d0b9a33a2afecf48e0e0d33e4498c50a45bf
varik <soiledz@gmail.com>
2024-09-19 00:34:20 +0300

    KS-2-1 change name variables

--- src/main/kotlin/lesson_2/KS-2-1.kt
+++ src/main/kotlin/lesson_2/KS-2-1.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_2
 
 fun main() {
-    val human = 4
-    val a: Float = 3.0F
-    val b: Float = 4.0F
-    val c: Float = 3.0F
-    val d: Float = 5.0F
+    val studentInClassroom = 4
+    val scoreFirstStudent: Float = 3.0F
+    val scoreSecondStudent: Float = 4.0F
+    val scoreThirdStudent: Float = 3.0F
+    val scoreFourthStudent: Float = 5.0F
 
-    val average: Float = (a + b + c + d) / human
+    val averageScore: Float = (scoreFirstStudent + scoreSecondStudent + scoreThirdStudent + scoreFourthStudent) / studentInClassroom
 
-    val formattedNumber = String.format("%.2f", average)
+    val printFormattedAverageScore = String.format("%.2f", averageScore)
 
-     println(formattedNumber)
+     println(printFormattedAverageScore)
 }
