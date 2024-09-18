464c7a7b91e4d32084ff6fcd65d578dd82e9b53c
varik <soiledz@gmail.com>
2024-09-19 00:49:54 +0300

    KS-2-4 create const variable

--- src/main/kotlin/lesson_2/KS-2-4.kt
+++ src/main/kotlin/lesson_2/KS-2-4.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_2
 
 fun main() {
+    val PERCENT: Int = 100
+
     val baf = 20
     val kristal = 7
     val iron = 11
 
-    val kristalBaf = (kristal * baf) / 100
-    val ironBaf = (iron * baf) / 100
+    val kristalBaf = (kristal * baf) / PERCENT
+    val ironBaf = (iron * baf) / PERCENT
 
     println(kristalBaf)
     println(ironBaf)
