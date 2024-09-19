fa79201a0478440367d5d38003e4d0ff6ff867cb
varik <soiledz@gmail.com>
2024-09-19 22:53:00 +0300

    KS-2-5 compound interest

---
+++ src/main/kotlin/lesson_2/KS-2-5.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+    val PERCENT = 100
+    val initialAmount = 70000.0
+    val annualInterestRate = 16.7 / PERCENT
+    val years = 20
+
+    val finalAmount = initialAmount * Math.pow(1 + annualInterestRate, years.toDouble())
+
+    println(String.format("%.3f", finalAmount))
+}
