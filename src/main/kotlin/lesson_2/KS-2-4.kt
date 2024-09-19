cffb0d46430773f205099aa4da3c5501438d6698
varik <soiledz@gmail.com>
2024-09-19 22:35:33 +0300

    KS-2-4 Baf

--- src/main/kotlin/lesson_2/KS-2-4.kt
+++ src/main/kotlin/lesson_2/KS-2-4.kt
@@ -1,14 +1,16 @@
 package org.example.lesson_2
 
 fun main() {
-    val PERCENT: Int = 100
+    val PERCENT = 100.0
 
     val baf = 20
     val kristal = 7
     val iron = 11
 
-    val kristalBaf = (kristal * baf) / PERCENT
-    val ironBaf = (iron * baf) / PERCENT
+    val bafKoef = baf / PERCENT
+
+    val kristalBaf = (kristal * bafKoef).toInt()
+    val ironBaf = (iron * bafKoef).toInt()
 
     println(kristalBaf)
     println(ironBaf)
