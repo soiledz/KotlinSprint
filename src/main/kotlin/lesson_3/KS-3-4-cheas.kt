865989e4c6b8a1e80d610a832250ff572785d4fb
varik <soiledz@gmail.com>
2024-09-22 17:10:04 +0300

    KS-3-4-cheas

---
+++ src/main/kotlin/lesson_3/KS-3-4-cheas.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+    var moveFrom = "E2"
+    var moveTo = "E4"
+    var moveNumber = 1
+
+    var moveString = "$moveFrom-$moveTo;$moveNumber"
+    println(moveString)
+
+    moveFrom = "D2"
+    moveTo = "D3"
+    moveNumber = 2
+
+    moveString = "$moveFrom-$moveTo;$moveNumber"
+    println(moveString)
+}
