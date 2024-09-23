5b2dd3829c3b27801c8eb69d43aeadc8800d4c72
Soiledz <soiledz@gmail.com>
2024-09-23 23:28:25 +0300

    Revert "KS-4-4"
    This reverts commit 626c576d3aed5642dce1a871ff2261bea92a9f04.

--- src/main/kotlin/lesson_4/KS-4-4.kt
+++ src/main/kotlin/lesson_4/KS-4-4.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_4
 
 fun main() {
-    val thisDay: Int = 4
+    val thisDay: Int = 5
 
     val armsAbs = thisDay % 2 != 0
+    val legsBack = thisDay % 2 == 0
 
     println("""
         Упражнения для рук:       $armsAbs
-        Упражнения для ног:       ${!armsAbs}
-        Упражнения для спины:     ${!armsAbs}
+        Упражнения для ног:       $legsBack
+        Упражнения для спины:     $legsBack
         Упражнения для пресса:    $armsAbs
     """.trimIndent())
 
