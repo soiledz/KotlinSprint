6624a663973d8965531ca8188a36f4e208d6901b
varik <soiledz@gmail.com>
2024-09-23 22:38:45 +0300

    KS-4-4

--- src/main/kotlin/lesson_4/KS-4-4.kt
+++ src/main/kotlin/lesson_4/KS-4-4.kt
@@ -1,15 +1,14 @@
 package org.example.lesson_4
 
 fun main() {
-    val thisDay: Int = 5
+    val thisDay: Int = 4
 
     val armsAbs = thisDay % 2 != 0
-    val legsBack = thisDay % 2 == 0
 
     println("""
         Упражнения для рук:       $armsAbs
-        Упражнения для ног:       $legsBack
-        Упражнения для спины:     $legsBack
+        Упражнения для ног:       ${!armsAbs}
+        Упражнения для спины:     ${!armsAbs}
         Упражнения для пресса:    $armsAbs
     """.trimIndent())
 
