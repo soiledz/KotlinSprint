f750d74a387cde3642d69c63a79f1d3d9e5e9356
varik <soiledz@gmail.com>
2024-09-23 21:33:58 +0300

    KS-3-5-change_split

--- src/main/kotlin/lesson_3/KS-3-5-cheas2.kt
+++ src/main/kotlin/lesson_3/KS-3-5-cheas2.kt
@@ -3,8 +3,7 @@ package org.example.lesson_3
 fun main() {
     val dataMove = "D2-D4;0"
 
-    val (move, moveNumber) = dataMove.split(";")
-    val (moveFrom, moveTo) = move.split("-")
+    val (moveFrom, moveTo, moveNumber) = dataMove.split("-",";")
 
     println(moveFrom)
     println(moveTo)
