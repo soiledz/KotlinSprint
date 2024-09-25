02ea3939d86d8f8c97ffe2c3c1d060860b0b2908
varik <soiledz@gmail.com>
2024-09-25 22:21:56 +0300

    KS-5-1

--- src/main/kotlin/lesson_5/KS-5-1.kt
+++ src/main/kotlin/lesson_5/KS-5-1.kt
@@ -1,9 +1,12 @@
 package org.example.lesson_5
 
 fun main() {
-    val sum = 5 + 2
+    val a = 5
+    val b = 2
+    val sum = a + b
 
-    val input = readLine()!!.toInt()
+    println("Докажи что ты человек и реши простой пример: $a + $b")
+    val input = readln().toInt()
 
     if (sum == input) println("Добро пожаловать!")
     else println("Доступ запрещен.")
